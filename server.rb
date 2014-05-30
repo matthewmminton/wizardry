2b0ddbd4ed468f292f92eac323d31159bcbc7eb6
Matthew <matthew.minton@gmail.com>
2014-05-29 22:38:35 -0400

    changed file to CSV, functioning page

--- server.rb
+++ server.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
+require 'csv'
 
 get '/' do
-  @articles = File.readlines('articles')
+  @articles = File.readlines('articles.csv')
   erb :index
+end
+
+post '/wizards' do
+  url = params['article_url']
+  title = params['article_title']
+  description = params['article_text']
+
+  CSV.open('articles.csv', 'wb') do |csv|
+    csv << [title, url, description]
+  end
+
+  redirect '/'
 end
